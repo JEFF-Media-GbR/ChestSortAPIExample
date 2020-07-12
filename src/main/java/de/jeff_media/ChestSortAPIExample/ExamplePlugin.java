3246e792eb6d02a3717edb519fed029583199342
mfnalex <1122571+mfnalex@users.noreply.github.com>
2020-07-12 03:49:48 +0200

    .

--- src/main/java/de/jeff_media/ChestSortAPIExample/ExamplePlugin.java
+++ src/main/java/de/jeff_media/ChestSortAPIExample/ExamplePlugin.java
@@ -1,22 +1,28 @@
 package de.jeff_media.ChestSortAPIExample;
 
+import de.jeff_media.ChestSortAPI.ChestSort;
+import de.jeff_media.ChestSortAPI.ChestSortAPI;
+import de.jeff_media.ChestSortAPI.ChestSortEvent;
+import de.jeff_media.ChestSortAPI.Sortable;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import de.jeff_media.ChestSort.ChestSortAPI;
-import de.jeff_media.ChestSort.ChestSortEvent;
-import de.jeff_media.ChestSort.ChestSortPlugin;
-
-public class ExamplePlugin extends JavaPlugin implements Listener {
+public class ExamplePlugin extends JavaPlugin implements Listener, CommandExecutor {
 	
 	ChestSortAPI chestSortAPI;
 	
 	public void onEnable() {
 		
-		ChestSortPlugin chestSort = (ChestSortPlugin) getServer().getPluginManager().getPlugin("ChestSort");
-		if(chestSort==null || !(chestSort instanceof ChestSortPlugin)) {
+		ChestSort chestSort = (ChestSort) getServer().getPluginManager().getPlugin("ChestSort");
+		if(chestSort==null) {
 			getLogger().severe("Error: ChestSort is not installed.");
 			return;
 		}
@@ -24,6 +30,7 @@ public class ExamplePlugin extends JavaPlugin implements Listener {
 		chestSortAPI = chestSort.getAPI();
 		
 		getServer().getPluginManager().registerEvents(this, this);
+		getCommand("chestsorttest").setExecutor(this);
 	}
 	
 	@EventHandler
@@ -39,6 +46,23 @@ public class ExamplePlugin extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
 		chestSortAPI.sortInventory(event.getPlayer().getInventory());
+	}
+
+	// Displays a custom inventory on command "/chestsorttest" to the player that can be sorted with hotkeys
+	public boolean onCommand​(CommandSender sender, Command command, String label, String[] args) {
+
+		if(!(sender instanceof Player)) {
+			sender.sendMessage("You must be player to run this command.");
+			return true;
+		}
+
+		Player p = (Player) sender;
+
+		Inventory sortableInventory = Bukkit.createInventory(new Sortable(), 9,"Sort me!");
+		p.sendMessage("If you have ChestSort hotkeys enabled, you can use them now in this custom inventory.");
+		p.openInventory(sortableInventory);
+
+		return true;
 	}
 
 }
